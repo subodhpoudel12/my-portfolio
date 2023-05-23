708189482047af495fd10738248f9e330f99f411
subodhpoudel12 <subodhpoudel1000@gmail.com>
2023-05-23 21:26:32 +0545

    about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./component/Navbar";
 import Home from "./component/Home";
+import About from "./component/About";
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      {/* <About/> */}
     </div>
   );
 }
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
           <button className="text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600">
             View Work
             <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight />
+              <HiArrowNarrowRight className="ml-3" />
             </span>
           </button>
         </div>
