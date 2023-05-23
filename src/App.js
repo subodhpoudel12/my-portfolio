5d8a315de2186e20d7c5c9448aecc038dc230483
subodhpoudel12 <subodhpoudel1000@gmail.com>
2023-05-24 01:12:50 +0545

    next

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./component/Navbar";
 import Home from "./component/Home";
 import About from "./component/About";
 import Skills from "./component/Skills";
+import Work from "./component/Work";
+import Contact from "./component/Contact";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Home />
       <About/>
       <Skills/>
+      <Work/>
+      <Contact/>
     </div>
   );
 }
--- src/component/skills.jsx
+++ src/component/skills.jsx
@@ -17,9 +17,11 @@ const Skills = () => {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-            experience
+            Skills
           </p>
-          <p>These are the technologies i have worked with.</p>
+          <p className="py-4">
+          These are the technologies i have worked with.
+          </p>
         </div>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
           <div className="shadow-md shadow-[#040c16]">
