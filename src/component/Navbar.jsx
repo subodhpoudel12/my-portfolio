cf6f93e6eaa4ba8c2115b4129b300066a63799cf
subodhpoudel12 <subodhpoudel1000@gmail.com>
2023-05-25 19:04:51 +0545

    final

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -17,29 +17,29 @@ const Navbar = () => {
       <div>
         <ul className="hidden md:flex">
           <li>
-          <Link to="home" spy={true} smooth={true} duration={500}>
-          Home
-        </Link>
+            <Link to="home" spy={true} smooth={true} duration={500}>
+              Home
+            </Link>
           </li>
           <li>
-          <Link to="about" spy={true} smooth={true} duration={500}>
-          About
-        </Link>
+            <Link to="about" spy={true} smooth={true} duration={500}>
+              About
+            </Link>
           </li>
           <li>
-          <Link to="skills" spy={true} smooth={true} duration={500}>
-          Skills
-        </Link>
+            <Link to="skills" spy={true} smooth={true} duration={500}>
+              Skills
+            </Link>
           </li>
           <li>
-          <Link to="work" spy={true} smooth={true} duration={500}>
-          Work
-        </Link>
+            <Link to="work" spy={true} smooth={true} duration={500}>
+              Work
+            </Link>
           </li>
           <li>
-          <Link to="contact" spy={true} smooth={true} duration={500}>
-          Contact
-        </Link>
+            <Link to="contact" spy={true} smooth={true} duration={500}>
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
@@ -56,29 +56,59 @@ const Navbar = () => {
         }
       >
         <li className="py-6 text-4xl">
-        <Link onClick={handleClick} to="home" spy={true} smooth={true} duration={500}>
-          Home
-        </Link>
+          <Link
+            onClick={handleClick}
+            to="home"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Home
+          </Link>
         </li>
         <li className="py-6 text-4xl">
-        <Link  onClick={handleClick} to="about" spy={true} smooth={true} duration={500}>
-          About
-        </Link>
+          <Link
+            onClick={handleClick}
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            About
+          </Link>
         </li>
         <li className="py-6 text-4xl">
-        <Link onClick={handleClick} to="skills" spy={true} smooth={true} duration={500}>
-          Skills
-        </Link>
+          <Link
+            onClick={handleClick}
+            to="skills"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Skills
+          </Link>
         </li>
         <li className="py-6 text-4xl">
-        <Link onClick={handleClick} to="work" spy={true} smooth={true} duration={500}>
-          Work
-        </Link>
+          <Link
+            onClick={handleClick}
+            to="work"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Work
+          </Link>
         </li>
         <li className="py-6 text-4xl">
-        <Link onClick={handleClick} to="contact" spy={true} smooth={true} duration={500}>
-          Contact
-        </Link>
+          <Link
+            onClick={handleClick}
+            to="contact"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Contact
+          </Link>
         </li>
       </ul>
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
