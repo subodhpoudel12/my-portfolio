c07d8f061ffe1baa83fc3382935b8a920fd39128
subodhpoudel12 <subodhpoudel1000@gmail.com>
2023-08-16 19:27:09 +0545

    final

--- src/component/skills.jsx
+++ src/component/skills.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-import javascript from "../assets/javascript.png";
-import css from "../assets/css.png";
-import github from "../assets/github.png";
-import html from "../assets/html.png";
-import mongodb from "../assets/mongo.png";
-import node from "../assets/node.png";
-import tailwind from "../assets/tailwind.png";
-import react from "../assets/react.png";
-import express from "../assets/express.png";
-import bootstrap from "../assets/bootstrap.png";
-import postman from "../assets/postman.png";
+import htmlIcon from "../assets/html.png";
+import cssIcon from "../assets/css.png";
+import javascriptIcon from "../assets/javascript.png";
+import reactIcon from "../assets/react.png";
+import expressIcon from "../assets/express.png";
+import mongodbIcon from "../assets/mongo.png";
+import nodeIcon from "../assets/node.png";
+import githubIcon from "../assets/github.png";
+import bootstrapIcon from "../assets/bootstrap.png";
+import tailwindIcon from "../assets/tailwind.png";
+import postmanIcon from "../assets/postman.png";
 
 const Skills = () => {
+  const skillsData = [
+    { name: "HTML", icon: htmlIcon },
+    { name: "CSS", icon: cssIcon },
+    { name: "JavaScript", icon: javascriptIcon },
+    { name: "React", icon: reactIcon },
+    { name: "Express", icon: expressIcon },
+    { name: "MongoDB", icon: mongodbIcon },
+    { name: "Node", icon: nodeIcon },
+    { name: "GitHub", icon: githubIcon },
+    { name: "Bootstrap", icon: bootstrapIcon },
+    { name: "Tailwind", icon: tailwindIcon },
+    { name: "Postman", icon: postmanIcon },
+  ];
+
   return (
     <div name="skills" className="bg-[#0a192f] text-gray-300">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
@@ -20,62 +34,16 @@ const Skills = () => {
             Skills
           </p>
           <p className="py-4">
-          These are the technologies i have worked with.
+            These are the technologies I have worked with.
           </p>
         </div>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16]">
-            <img className="w-20 mx-auto" src={html} alt="HTML icon" />
-            <p>HTML</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16]">
-            <img className="w-20 mx-auto" src={css} alt="CSS icon" />
-            <p>CSS</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16]">
-            <img
-              className="w-20 mx-auto"
-              src={javascript}
-              alt="JavaScript icon"
-            />
-            <p>JavaScript</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16]">
-            <img className="w-20 mx-auto" src={react} alt="React icon" />
-            <p>React</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16]">
-            <img className="w-20 mx-auto" src={express} alt="Express icon" />
-            <p>Express</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16]">
-            <img className="w-20 mx-auto" src={mongodb} alt="MongoDB icon" />
-            <p>MongoDB</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16]">
-            <img className="w-20 mx-auto" src={node} alt="Node icon" />
-            <p>Node</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16]">
-            <img className="w-20 mx-auto" src={github} alt="GitHub icon" />
-            <p>GitHub</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16]">
-            <img
-              className="w-20 mx-auto"
-              src={bootstrap}
-              alt="Bootstrap icon"
-            />
-            <p>Bootstrap</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16]">
-            <img className="w-20 mx-auto" src={tailwind} alt="TailWind icon" />
-            <p>TailWind</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16]">
-            <img className="w-20 mx-auto" src={postman} alt="Postman icon" />
-            <p>Postman</p>
-          </div>
+          {skillsData.map((skill, index) => (
+            <div key={index} className="shadow-md shadow-[#040c16]">
+              <img className="w-20 mx-auto" src={skill.icon} alt={`${skill.name} icon`} />
+              <p>{skill.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
